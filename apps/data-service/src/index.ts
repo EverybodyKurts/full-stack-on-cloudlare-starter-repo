fe243c04882c65aaf02f48e1222d778b19bedb49
Kurt Mueller <kurtronaldmueller@gmail.com>
2025-10-03 09:17:24 -0400

    Start to handle queue messages

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,6 +1,7 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 import { App } from './hono/app';
 import { initDatabase } from '@repo/data-ops/database';
+import { QueueMessageSchema } from '@repo/data-ops/zod-schema/queue';
 
 export default class DataService extends WorkerEntrypoint<Env> {
   constructor(ctx: ExecutionContext, env: Env) {
@@ -16,6 +17,16 @@ export default class DataService extends WorkerEntrypoint<Env> {
   async queue(batch: MessageBatch<unknown>): Promise<void> {
     for (const message of batch.messages) {
       console.log('Message Body:', message.body);
+      const parsedMessage = QueueMessageSchema.safeParse(message.body);
+      if (parsedMessage.success) {
+        // Process the valid message
+        console.log('Valid message:', parsedMessage.data);
+        const event = parsedMessage.data;
+        if (event.type === 'LINK_CLICK') {
+        }
+      } else {
+        console.error('Invalid message format:', parsedMessage.error);
+      }
     }
   }
 }
