9e94bd164dfa31177b0cd3abd60b7897c68909ec
Kurt Mueller <kurtronaldmueller@gmail.com>
2025-10-06 09:35:29 -0400

    Add endpoint to increment and retrieve count from EvaluationScheduler durable object

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono';
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
 import { getDestinationForCountry, getRoutingDestination } from '@/helpers/route-ops';
 import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
+import { EvaluationScheduler } from '..';
 
 export const App = new Hono<{ Bindings: Env }>();
 
@@ -42,4 +43,15 @@ App.get('/:id', async (c) => {
   c.executionCtx.waitUntil(c.env.QUEUE.send(queueMessage));
 
   return c.redirect(destination);
+});
+
+App.get('/do/:name', async (c) => {
+  const name = c.req.param('name');
+  const doId: DurableObjectId = c.env.EVALUATION_SCHEDULER.idFromName(name);
+  const stub: DurableObjectStub<EvaluationScheduler> = c.env.EVALUATION_SCHEDULER.get(doId);
+
+  await stub.increment();
+  const count = await stub.getCount();
+
+  return c.json({ count });
 });
