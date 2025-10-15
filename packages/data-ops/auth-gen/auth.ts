5994ce8eed128b967430e259b5ba162eed838933
Kurt Mueller <kurtronaldmueller@gmail.com>
2025-10-15 09:51:14 -0400

    More auth config

--- packages/data-ops/auth-gen/auth.ts
+++ packages/data-ops/auth-gen/auth.ts
@@ -1,7 +1,5 @@
 import { Auth, createBetterAuth } from "@/auth";
-import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import Database from "better-sqlite3";
 
 export const auth: Auth = createBetterAuth(
   drizzleAdapter({}, { provider: "sqlite" })
